6977565e81dff7c8c9cf973ce70c6086b7a290eb
Tle <zxtle.44@gmail.com>
2022-04-07 16:17:01 +0700

    template_OnePage

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const chalk = require('chalk')
 const debug = require('debug')('app') // debug show GET/Request
 const morgan = require('morgan')// middleware การทำงานของ web app
 const app = express()
+const path = require('path')
 const port = 3001
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/"))) //dirname คือ directory ปัจจุบัน // บ่งบอกว่า static file อยู่ที่ไหน
 
 app.get("/",(req,res) =>{
     res.send('Hello Tle')
