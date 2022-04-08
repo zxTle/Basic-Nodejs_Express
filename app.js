2cec4611dd8cadcd97b1dba763132b24e3897f99
Tle <zxtle.44@gmail.com>
2022-04-08 15:05:42 +0700

    ejs

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const debug = require('debug')('app') // debug show GET/Request
 const morgan = require('morgan')// middleware การทำงานของ web app
 const app = express()
 const path = require('path')
-const port = 3001
+const port = process.env.PORT; // set environment ไว้ใน package.json
 
 app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname,"/public/"))) //dirname คือ directory ปัจจุบัน // บ่งบอกว่า static file อยู่ที่ไหน
 
 app.get("/",(req,res) =>{
-    res.send('Hello Tle')
+    res.render('index',{username : 'Tle'})
 })  // getไว้จัดการ request ตาม 
+
+app.set("views","./src/views")
+app.set("view engine","ejs")
 
 app.listen(port,()=>{
     console.log('listening on port'+ chalk.green(port))
